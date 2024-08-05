af6754613763209b4ea89ea98aac6732944a9ced
Julio Sanic <julio.sanic.gt.256@gmail.com>
2024-08-05 15:15:16 -0600

    navbar scroll

--- src/sections/navbar/Navbar.tsx
+++ src/sections/navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 
 import logo from "@/../public/images/logo.svg";
@@ -6,10 +7,37 @@ import { FaFacebook, FaTwitter } from "react-icons/fa6";
 
 import "./navbar.css";
 import MobileMenu from "./MobileMenu";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [bgOnScroll, setBgOnScroll] = useState(false);
+
+  useEffect(() => {
+    const changeBackgroundOnScroll = () => {
+      if (typeof window !== "undefined") {
+        if (window.scrollY > 0) {
+          setBgOnScroll(true);
+        } else {
+          setBgOnScroll(false);
+        }
+      }
+    };
+
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", changeBackgroundOnScroll);
+    }
+
+    return () => {
+      if (typeof window !== "undefined") {
+        window.removeEventListener("scroll", changeBackgroundOnScroll);
+      }
+    };
+  }, []);
+
+  console.log(bgOnScroll);
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${bgOnScroll && "navbar--scroll"}`}>
       <div className="navbar-flex container">
         <Image src={logo} alt="logo" />
         <div className="main-menu-items">
